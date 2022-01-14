1d5fd91ebd59be82d46e2d07e75862a69fee551f
Sergey Gusterev <copywriter-sg@yandex.ru>
2022-01-14 10:34:47 +0300

    доработать переполнение текста

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -5,20 +5,25 @@ const inputName = formElement.querySelector('.popup__name');
 const inputJob = formElement.querySelector('.popup__job');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
-const valueName = inputName.setAttribute('value', profileName.textContent);
-const valueJob = inputJob.setAttribute('value', profileJob.textContent);
+const popupClose = popup.querySelector('.popup__close');
+
+inputName.setAttribute('value', profileName.textContent);
+inputJob.setAttribute('value', profileJob.textContent);
 
 btnEditProfile.addEventListener('click', function () {
   popup.classList.add('popup_opened');
-  const popupClose = popup.querySelector('.popup__close');
+  inputName.value = inputName.getAttribute('value');
+  inputJob.value = inputJob.getAttribute('value');
+})
 
-  popupClose.addEventListener('click', function () {
-    popup.classList.remove('popup_opened');
-  })
+popupClose.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
 })
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
+  inputName.setAttribute('value', inputName.value);
+  inputJob.setAttribute('value', inputJob.value);
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   popup.classList.remove('popup_opened');
