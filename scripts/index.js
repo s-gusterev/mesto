9d04fb8e8e45348af0e6734d12f90afe44a0e9f8
Sergey Gusterev <copywriter-sg@yandex.ru>
2022-01-30 18:27:06 +0300

    исправить функции добавления карточек в разметку

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const popupProfile = document.querySelector('.popup_type_profile');
 const popupAddCard = document.querySelector('.popup_type_card-add');
 const popupFullImage = document.querySelector('.popup_type_picture');
 const popup = document.querySelectorAll('.popup');
-const popupClose = document.querySelector('.popup__close');
+const popupCloseProfile = document.querySelector('.popup__close_type_profile');
 const popupClosePlace = document.querySelector('.popup__close_type_place');
 const popupCloseImage = document.querySelector('.popup__close_type_image');
 const popupImage = document.querySelector('.popup__img');
@@ -20,14 +20,13 @@ const profileJob = document.querySelector('.profile__subtitle');
 const cards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card').content;
 
-// Открытие, закрытие попапа
-function openClosePopup(popup) {
-  popup.classList.toggle('popup_opened');
+// Открытие попапа
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
-
-// Закрытие попапа по крестику
-function closePopup(evt) {
-  evt.target.parentElement.parentElement.classList.remove('popup_opened');
+// Закрытие попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 //Функция лайка карточки
@@ -40,7 +39,7 @@ function openFullImage(evt) {
   const image = evt.target;
   popupImage.src = image.src;
   popupImageDescription.textContent = image.alt;
-  openClosePopup(popupFullImage);
+  openPopup(popupFullImage);
 }
 
 //Функция удаления карточки
@@ -53,7 +52,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  openClosePopup(popupProfile);
+  closePopup(popupProfile);
 }
 
 // Функция формы добавления карточки
@@ -61,19 +60,25 @@ function formAddCardSubmit(evt) {
   evt.preventDefault();
   const title = inputCardTitle.value;
   const image = inputCardImage.value;
-  addCard(title, image);
-  openClosePopup(popupAddCard);
+  const cardRender = templateCard(title, image);
+  addCard(cardRender);
+  closePopup(popupAddCard);
 }
 
 // Функция рендеринга карточек из массива
 function renderCards() {
   initialCards.forEach(function (card) {
-    addCard(card.name, card.link);
+    const cardRender = templateCard(card.name, card.link);
+    addCard(cardRender);
   })
 };
+// Добавление карточки в разметку
+function addCard(card) {
+  cards.prepend(card)
+}
 
-// Функция добавления карточки
-function addCard(title, img) {
+// Функция шаблона карточки
+function templateCard(title, img) {
   const cardBody = cardTemplate.querySelector('.card').cloneNode(true);
   const cardTitle = cardBody.querySelector('.card__title');
   const cardImg = cardBody.querySelector('.card__img');
@@ -82,11 +87,11 @@ function addCard(title, img) {
   cardTitle.textContent = title;
   cardImg.setAttribute('src', img);
   cardImg.setAttribute('alt', title);
-  cards.prepend(cardBody);
 
   cardLike.addEventListener('click', addCardLike);
   cardDel.addEventListener('click', deleteCard);
   cardImg.addEventListener('click', openFullImage);
+  return cardBody;
 }
 
 renderCards();
@@ -94,17 +99,17 @@ renderCards();
 btnEditProfile.addEventListener('click', function () {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
-  openClosePopup(popupProfile);
+  openPopup(popupProfile);
 })
 
 btnAddCard.addEventListener('click', function () {
   inputCardTitle.value = '';
   inputCardImage.value = '';
-  openClosePopup(popupAddCard);
+  openPopup(popupAddCard);
 })
 
-popupClose.addEventListener('click', closePopup);
-popupClosePlace.addEventListener('click', closePopup);
-popupCloseImage.addEventListener('click', closePopup);
+popupCloseProfile.addEventListener('click', () => closePopup(popupProfile));
+popupClosePlace.addEventListener('click', () => closePopup(popupAddCard));
+popupCloseImage.addEventListener('click', () => closePopup(popupFullImage));
 formEditProfile.addEventListener('submit', formSubmitHandler);
 formAddCard.addEventListener('submit', formAddCardSubmit);
